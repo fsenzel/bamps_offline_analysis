b6f839f25d108b2a1800c2a55237c8197663cc50
uphoff <uphoff@mycompany.com>
2012-03-09 15:51:58 +0000

    offlineAnalysis/trunk: copy doxygen cmake file from full/trunk since in the latter version it is not necessary anymore to have doxygen installed.

--- ConfigureDoxygenTarget.cmake
+++ ConfigureDoxygenTarget.cmake
@@ -4,24 +4,25 @@
 
 ######### check whether doxygen is installed #########
 FIND_PACKAGE(Doxygen)
-IF (DOXYGEN_FOUND STREQUAL "NO")
-  MESSAGE(FATAL_ERROR "!! Doxygen not found.")
-ELSE()
-  MESSAGE(STATUS "** Found doxygen")
-ENDIF (DOXYGEN_FOUND STREQUAL "NO")
+
+IF (DOXYGEN_FOUND)
 
 ######### prepare doxygen configuration file #########
-CONFIGURE_FILE(${CMAKE_CURRENT_SOURCE_DIR}/Doxyfile.in ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile)
+  CONFIGURE_FILE(${CMAKE_CURRENT_SOURCE_DIR}/Doxyfile.in ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile)
 
 ######### add doxygen as target #########
-ADD_CUSTOM_TARGET(doxygen ${DOXYGEN_EXECUTABLE} ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile)
+  ADD_CUSTOM_TARGET(doxygen ${DOXYGEN_EXECUTABLE} ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile)
 
 ######### cleanup $build/doc on "make clean" #########
-SET_PROPERTY(DIRECTORY APPEND PROPERTY ADDITIONAL_MAKE_CLEAN_FILES doc)
+  SET_PROPERTY(DIRECTORY APPEND PROPERTY ADDITIONAL_MAKE_CLEAN_FILES doc)
 
 #########  add doxygen as dependency to doc-target ######### 
-GET_TARGET_PROPERTY(DOC_TARGET doc TYPE)
-IF(NOT DOC_TARGET)
-  ADD_CUSTOM_TARGET(doc)
-ENDIF()
-ADD_DEPENDENCIES(doc doxygen)
+  GET_TARGET_PROPERTY(DOC_TARGET doc TYPE)
+  IF(NOT DOC_TARGET)
+    ADD_CUSTOM_TARGET(doc)
+  ENDIF()
+  ADD_DEPENDENCIES(doc doxygen)
+
+ELSE (DOXYGEN_FOUND)
+  MESSAGE(STATUS "no doxygen target created")
+ENDIF (DOXYGEN_FOUND)
