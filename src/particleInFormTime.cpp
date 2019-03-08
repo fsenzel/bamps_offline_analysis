821659b93a80531b6777cd2053dbad8a310f2c30
senzel <senzel@mycompany.com>
2019-03-08 14:54:16 +0000

    full/offlineAnalysis/branches/stochasticLPM: added files forgotten in previous commit.

---
+++ src/particleInFormTime.cpp
@@ -0,0 +1,19 @@
+//---------------------------------------------------------------------------------------
+//provided by subversion
+//---------------------------------------------------------------------------------------
+//$HeadURL: svn+ssh://senzel@th.physik.uni-frankfurt.de/home/bamps/svn/full/offlineAnalysis/branches/stochasticLPM/src/particleOffline.cpp $
+//$LastChangedDate: 2015-03-12 15:13:19 +0100 (Do, 12 Mär 2015) $
+//$LastChangedRevision: 2121 $
+//$LastChangedBy: senzel $
+//---------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------
+
+/** @file 
+ * @brief definitions for the ParticleInFormTime class
+ */
+
+/** @author Florian Senzel */
+
+
+#include "particleInFormTime.h"
+
---
+++ src/particleInFormTime.h
@@ -0,0 +1,75 @@
+//--------------------------------------------------------- -*- c++ -*- ------
+//provided by subversion
+//----------------------------------------------------------------------------
+//$HeadURL: file:///home/bamps/svn/full/offlineAnalysis/branches/stochasticLPM/src/particleOffline.h $
+//$LastChangedDate: 2019-03-06 15:54:36 +0100 (Wed, 06. Mar 2019) $
+//$LastChangedRevision: 2944 $
+//$LastChangedBy: senzel $
+//----------------------------------------------------------------------------
+//----------------------------------------------------------------------------
+
+/** @file
+ * @brief Declarations for the Offline Particle class
+ */
+
+#ifndef PARTICLEINFORMTIME_H
+#define PARTICLEINFORMTIME_H
+
+#include <vector>
+#include <limits>
+
+#include "particle.h"
+#include "globalsettings.h"
+
+class ParticleInFormTime : public Particle
+{
+public:
+  // /** @brief Provide standard constructor (for completeness) */
+  // ParticleInFormTime( const double ) : 
+  //   Particle(), 
+  //   tInitEmission( -1.0 ),
+  //   deltaMomMother( VectorEPxPyPz( 0.0, 0.0, 0.0, 0.0 ) ),
+  //   MomAtEmission( VectorEPxPyPz( 0.0, 0.0, 0.0, 0.0 ) )
+  //   {};
+    
+  ParticleInFormTime( const Particle& _particle, const double _tInitEmission, const VectorEPxPyPz _deltaMomMother, const VectorEPxPyPz _momAtEmission ) : 
+    Particle( _particle ), 
+    tInitEmission( _tInitEmission ),
+    deltaMomMother( _deltaMomMother ),
+    MomAtEmission( _momAtEmission ),
+    NscattDuringTau( 1.0 )
+  {};
+    
+  double NscattDuringTau;
+
+  VectorEPxPyPz getDeltaMomMother() const { return deltaMomMother; };
+  VectorEPxPyPz getMomAtEmission() const { return MomAtEmission; };
+
+  double getFormationTimeCoherentState( const Particle _mother ) const
+  {
+    const double kt2 = this->Mom.TransverseMomentumToVectorSquared( _mother.Mom );
+
+    if( kt2 == 0.0 )
+      return std::numeric_limits<double>::infinity();
+    else
+    {
+      return this->tInitEmission + ( this->Mom.E() / kt2 * ns_casc::InvGevToFm ); // fm
+    }
+  };
+
+private:
+
+  double tInitEmission;
+  VectorEPxPyPz deltaMomMother;
+  VectorEPxPyPz MomAtEmission;
+  
+};
+
+
+#ifdef BAMPS_DECLARE_ALLOCATOR
+BAMPS_DECLARE_ALLOCATOR(ParticleInFormTime);
+#else
+#warning "ALLOCATOR not defined"
+#endif
+
+#endif
